f266d1405da4cf16a629a87b376774854956f35c
geoffreylarnold <geoffrey.arnold@alleghenycounty.us>
2025-07-30 14:12:26 -0400

    Add date generated text function

--- R/quarto.R
+++ R/quarto.R
@@ -29,3 +29,29 @@ kpi_callout <- function(type, kpi, text) {
   ) %>%
     cat()
 }
+
+#' Function to generate the proper date generated text.
+#'
+#' @return HTML Date Rendered Text
+#' @param page Page type of rendered text, default = "Dept" which includes entire text, any other value will just provide the Month, Day and Year as text.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' ```{r results='asis'}
+#' gen_date_readable()
+#' ```
+#' }
+gen_date_readable <- function(page = "Dept") {
+  mon <- format(Sys.Date(), "%B")
+  year <- format(Sys.Date(), "%Y")
+  day <- as.numeric(format(Sys.Date(), "%d"))
+  day_form <- scales::label_ordinal()(as.integer(day))
+  if (page == "Dept") {
+    txt <- glue::glue("<small>Page generated on {mon} {day_form}, {year}</small>")
+  } else {
+    txt <- glue::glue("{mon} {day_form}, {year}")
+  }
+  return(txt)
+}
